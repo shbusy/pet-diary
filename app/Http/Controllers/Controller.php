e3b30c44ba77193f5097d9267aa855431a0ffd91
limsuhyun <shbusy1234@gmail.com>
2023-06-19 19:21:49 +0900

    feat : add index

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\DB;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    // 메인 진입시 로그인여부 처리
+    public function index() {
+        $msg = "";
+        if (auth()->guest()) {
+            $msg = "guest";
+        } else if(auth()->check()) {
+            $msg = "member";
+        }
+
+        return view('welcome')
+            ->with([
+                'msg' => $msg,
+            ]);
+    }
 }
