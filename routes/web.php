18cf1fdda84554fee44573c99f5aeffb640dc7ec
limsuhyun <shbusy1234@gmail.com>
2023-06-19 19:28:18 +0900

    feat : change index router

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', [Controller::class, 'index']);
+
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
