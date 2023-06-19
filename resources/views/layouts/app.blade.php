a15f7a74c739caa7de472fc42439b9a82c33eb1e
limsuhyun <shbusy1234@gmail.com>
2023-06-19 09:48:56 +0900

    feat : add jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <script  src="http://code.jquery.com/jquery-latest.min.js"></script>
         <title>@yield('title')</title>
     </head>
     <body>
