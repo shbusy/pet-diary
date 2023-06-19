13e9390be4e3c884e4148da92b149224d595ae4c
limsuhyun <shbusy1234@gmail.com>
2023-06-19 19:25:58 +0900

    feat : add user authentication in welcome.blade layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,15 @@
 
 @section('content')
     <h1>main</h1>
-    <a href="{{ route('register') }}">회원가입</a>
-    <a href="">로그인</a>
+    @if($msg == "member")
+        {{ Auth::user()->name }}님 안녕하세요
+        <form action="{{ route('logout') }}" method="POST">
+            @csrf
+            <button type="submit">로그아웃</button>
+        </form>
+    @else
+        <a href="{{ route('register') }}">회원가입</a>
+        <a href="{{ route('login') }}">로그인</a>
+    @endif
+
 @endsection
