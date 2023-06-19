1e27d68dfc6b6147990ed34cdf21bf961df5e185
limsuhyun <shbusy1234@gmail.com>
2023-06-19 20:50:47 +0900

    feat : add relationship on users and blogs

---
+++ app/Models/Blog.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'display_name'
+    ];
+
+    protected function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    // /blogs/{blog} 를 원하는 값으로 변경
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
+}
