55a9a79829411c03c77505a88cbbd9ddf64f98d6
limsuhyun <shbusy1234@gmail.com>
2023-06-21 00:38:13 +0900

    Revert "feat: setting PostController"
    This reverts commit 53960d10a4157f66cf8ecaaaa80879cd74506127.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,93 +2,65 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Blog;
 use App\Models\Post;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 
 class PostController extends Controller
 {
-    public function __construct () {
-        $this->authorizeResource(Post::class, 'post', [
-            'except' => ['create', 'store']
-        ]);
-
-        $this->middleware('can:create,App\Models\Post,blog')
-            ->only(['create', 'store']);
-    }
-
     /**
-     * 글 목록
+     * Display a listing of the resource.
      */
-    public function index(Blog $blog)
+    public function index()
     {
-        return view('blogs.posts.index', [
-            'posts' => $blog->posts()->latest()->paginate()
-        ]);
+        //
     }
 
     /**
-     * 글쓰기 폼
+     * Show the form for creating a new resource.
      */
-    public function create(Blog $blog)
+    public function create()
     {
-        return view('blogs.posts.create', [
-            'blog' => $blog
-        ]);
+        //
     }
 
     /**
-     * 글쓰기
+     * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request, Blog $blog)
+    public function store(StorePostRequest $request)
     {
-        $post = $blog->posts()->create(
-            $request->only(['title', 'content'])
-        );
-
-        return to_route('posts.show', $post);
+        //
     }
 
     /**
-     * 글 본문
+     * Display the specified resource.
      */
     public function show(Post $post)
     {
-        return view('blogs.posts.show', [
-            'post' => $post
-        ]);
+        //
     }
 
     /**
-     * 글 수정 폼
+     * Show the form for editing the specified resource.
      */
     public function edit(Post $post)
     {
-        return view('blogs.posts.edit' ,[
-            'post' => $post
-        ]);
+        //
     }
 
     /**
-     * 글 수정
+     * Update the specified resource in storage.
      */
     public function update(UpdatePostRequest $request, Post $post)
     {
-        $post->update(
-            $request->only(['title', 'content'])
-        );
-
-        return to_route('posts.show', $post);
+        //
     }
 
     /**
-     * 글 삭제
+     * Remove the specified resource from storage.
      */
     public function destroy(Post $post)
     {
-        $post -> delete();
-
-        return to_route('blogs.posts.index', $post -> blog);
+        //
     }
 }
