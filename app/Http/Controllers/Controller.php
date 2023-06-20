66885e1e0f7eecaa68b5344c38d754162bd6a073
limsuhyun <shbusy1234@gmail.com>
2023-06-21 00:33:52 +0900

    feat: add Blog model in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -12,7 +13,7 @@ class Controller extends BaseController
     use AuthorizesRequests, ValidatesRequests;
 
     // 메인 진입시 로그인여부 처리
-    public function index() {
+    public function index(Blog $blog) {
         $msg = "";
         if (auth()->guest()) {
             $msg = "guest";
