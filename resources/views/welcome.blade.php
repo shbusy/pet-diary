1cfac38b9fb3632d4e469a81644645a2329c7b1e
limsuhyun <shbusy1234@gmail.com>
2023-06-20 21:22:48 +0900

    feat: add subscription

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,11 @@
 @section('content')
     <h1>main</h1>
     @if($msg == "member")
-        {{ Auth::user()->name }}님 안녕하세요
+        {{ auth()->user()->name }}님 안녕하세요
         <form action="{{ route('logout') }}" method="POST">
             @csrf
             <button type="submit">로그아웃</button>
+            <a href="{{ route('blogs.index') }}">내 피드</a>
         </form>
     @else
         <a href="{{ route('register') }}">회원가입</a>
