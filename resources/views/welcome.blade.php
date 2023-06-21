142dee32c993e51cd0829d6aa3411d86a8c856a3
limsuhyun <shbusy1234@gmail.com>
2023-06-21 22:16:18 +0900

    feat: get random post in welcome.blade layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,17 +3,8 @@
 @section('title', '메인')
 
 @section('content')
-    <h1>main</h1>
-    @if($msg == "member")
-        {{ auth()->user()->name }}님 안녕하세요
-        <form action="{{ route('logout') }}" method="POST">
-            @csrf
-            <button type="submit">로그아웃</button>
-            <a href="{{ route('blogs.index') }}">내 피드</a>
-        </form>
-    @else
-        <a href="{{ route('register') }}">회원가입</a>
-        <a href="{{ route('login') }}">로그인</a>
-    @endif
-
+    @foreach ($randomPosts as $post)
+        <h3>{{ $post->title }}</h3>
+        <p>{{ $post->content }}</p>
+    @endforeach
 @endsection
