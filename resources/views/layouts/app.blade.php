bcd630f93ba106524e0733730daad007e1f6b7cf
limsuhyun <shbusy1234@gmail.com>
2023-06-22 20:33:10 +0900

    feat: change header name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <script  src="http://code.jquery.com/jquery-latest.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/minty/bootstrap.min.css">
-        <title>@yield('title')</title>
+        <title>Pet-Diary</title>
     </head>
 
     <body>
@@ -19,7 +19,7 @@
                         <ul class="navbar-nav me-auto">
                     @auth
                             <li class="nav-item">
-                                <a class="nav-link" href="{{ route('blogs.index') }}">my-feed</a>
+                                <a class="nav-link" href="{{ route('blogs.index') }}">MyFeed</a>
                             </li>
                     @endauth
                         </ul>
@@ -35,16 +35,11 @@
                         <button type="button" style="margin-right: 5px" class="btn btn-primary" onclick="location.href='{{ route('login') }}'">login</button>
                         <button type="button" style="margin-right: 5px" class="btn btn-primary" onclick="location.href='{{ route('register') }}'">join</button>
                     @endguest
-                    </p>
-{{--                    <form class="d-flex">
-                        <input class="form-control me-sm-2" type="search" placeholder="Search">
-                        <button class="btn btn btn-primary my-2 my-sm-0" type="submit">Search</button>
-                    </form>--}}
                 </div>
             </div>
         </div>
-        <div style="padding-top:120px" class="container">
-            <div class="page-header">
+        <div style="padding-top:50px" class="container">
+            <div class="page-header" style="margin-bottom: 20px;">
                 <h1>@yield('header')</h1>
             </div>
             <div class="bs-docs-section">
