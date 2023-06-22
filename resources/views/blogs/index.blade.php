2ec87e72ad7d9dac9717be62933ca20aeaabadad
limsuhyun <shbusy1234@gmail.com>
2023-06-22 20:36:40 +0900

    feat: add bootstrap

--- resources/views/blogs/index.blade.php
+++ resources/views/blogs/index.blade.php
@@ -1,14 +1,38 @@
 @extends('layouts.app')
 
-@section('title', '블로그 목록')
+@section('header', 'MyFeed')
 
 @section('content')
-    <a href="{{ route('blogs.create') }}">블로그만들기</a>
-    <ul>
+
+    <div class="bs-component">
         @foreach ($blogs as $blog)
-            <h3><a href="{{ route('blogs.show', $blog) }}">{{ $blog->display_name }}</a></h3>
-            <div>{{ $blog->user->name }}</div>
+            <div class="card border-primary mb-3" style="width: 100%;display: inline-block;">
+                <div class="card-body" style="display: inline-block;width: 30%;float: left;">
+                    <h4 class="card-title"><a href="{{ route('blogs.show', $blog) }}">{{ $blog->display_name }}</a></h4>
+                    <h6 class="card-title"><a href="{{ route('blogs.show', $blog) }}" style="text-decoration: none;">{{ $blog->name }}</a></h6>
+                    <p class="text-body" style="margin-top: 20px;"><small>{{ $blog->description }}</small></p>
+                </div>
+                @php
+                    $post = $blog->posts()->orderBy('created_at', 'desc')->limit(4)->get();
+                    $post_cnt = count($post);
+                @endphp
+                @if($post_cnt > 0)
+                    @foreach($post as $post)
+                        <div class="card-body" style="display: inline-block; width: 17%; float: left;">
+                            <a href="{{ route('posts.show', $post) }}"><img src="{{ '/images/'. $post->img_link }}" style="width: 72%; border-radius: 70%; margin: 10px" alt="images"></a>
+                            <a href="{{ route('posts.show', $post) }}" style="text-decoration: none;"><p class="text-body"><small>{{ $post->title }}</small></p></a>
+                        </div>
+                    @endforeach
+                @else
+                    <div class="card-body" style="display: inline-block; width: 48%; float: left;">
+                        <p class="text-body-secondary">No posts</p>
+                    </div>
+                @endif
+            </div>
         @endforeach
-    </ul>
+        <div class="d-grid gap-2">
+            <button class="btn btn-lg btn-primary" type="button" onclick="location.href='{{ route('blogs.create') }}'">+ New Feed</button>
+        </div>
+    </div>
 
 @endsection
