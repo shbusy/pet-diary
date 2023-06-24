d3ac8c9e86f1e186d75ae970f7156bad44de3533
limsuhyun <shbusy1234@gmail.com>
2023-06-25 01:23:46 +0900

    feat: add image link, input tag in edit.blade layout

--- resources/views/blogs/posts/create.blade.php
+++ resources/views/blogs/posts/create.blade.php
@@ -15,16 +15,32 @@
 
         <div class="form-group">
             <label for="content" class="form-label mt-4">content</label>
-            <textarea class="form-control" id="content" rows="3" name="content" required>{{ old('content') }}</textarea>
+            <textarea class="form-control" id="content" rows="3" placeholder="content" name="content" required>{{ old('content') }}</textarea>
         </div>
 
-        <div class="form-group">
-            <label for="img" class="form-label mt-4"></label>
-            <input class="form-control" type="file" id="img" name="img" multiple>
+        <div class="form-group" style="margin-top: 10px;">
+            <button type="button" class="btn btn-light" id="add_file" style="float: left; margin-right: 10px;">파일첨부</button>
+            <input class="form-control" type="text" id="img_text" style="width: 91%; float: left;" name="img_text" value="{{ old('img_link') }}" multiple>
         </div>
+        <input class="form-control" type="file" id="img" name="img" value="{{ old('img_link') }}" style="display: none" multiple>
 
         <p class="bs-component">
             <button style="margin-top:20px" type="submit" class="btn btn-primary">Submit</button>
         </p>
     </form>
+
+    <script>
+        $(function() {
+            $("#img").change(function (){
+                var file = this.files[0];
+
+                console.log(file.name);
+                $("#img_text").val(file.name);
+            })
+
+            $("#add_file").click(function() {
+                $("#img").click();
+            })
+        })
+    </script>
 @endsection
--- resources/views/blogs/posts/edit.blade.php
+++ resources/views/blogs/posts/edit.blade.php
@@ -5,28 +5,51 @@
 @section('header', 'Edit Posts')
 
 @section('content')
-    <form action="{{ route('posts.update', $post) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
+    <div class="col-lg-12">
+        <form action="{{ route('posts.update', $post) }}" method="POST" id="edit_form" enctype="multipart/form-data">
+            @csrf
+            @method('PUT')
 
-        <div class="form-group">
-            <label class="col-form-label mt-4" for="title">title</label>
-            <input type="text" class="form-control" placeholder="title" id="title" name="title" value="{{ old('title', $post->title) }}" required autofocus>
-        </div>
+            <div class="form-group">
+                <label class="col-form-label mt-4" for="title">title</label>
+                <input type="text" class="form-control" placeholder="title" id="title" name="title" value="{{ old('title', $post->title) }}" required autofocus>
+            </div>
 
-        <div class="form-group">
-            <label for="content" class="form-label mt-4">content</label>
-            <textarea class="form-control" id="content" rows="3" name="content" required>{{ old('title', $post->content) }}</textarea>
-        </div>
+            <div class="form-group">
+                <label for="content" class="form-label mt-4">content</label>
+                <textarea class="form-control" id="content" rows="3" name="content" required>{{ old('content', $post->content) }}</textarea>
+            </div>
 
-        <div class="form-group">
-            <label for="img" class="form-label mt-4"></label>
-            <input class="form-control" type="file" id="img" name="img" multiple>
-        </div>
+            <div class="form-group" style="margin-top: 10px;">
+                <button type="button" class="btn btn-light" id="add_file" style="float: left; margin-right: 10px;">파일첨부</button>
+                <input class="form-control" type="text" id="img_text" style="width: 91%; float: left;" name="img_text" value="{{ old('title', $post->img_link) }}" multiple>
+            </div>
+                <input class="form-control" type="file" id="img" name="img" value="{{ old('title', $post->img_link) }}" style="display: none" multiple>
+        </form>
 
         <p class="bs-component">
-            <button style="margin-top:20px" type="submit" class="btn btn-primary">Submit</button>
+            <button style="margin-top:20px" type="submit" onclick="if(confirm('수정하시겠습니까?')) { $('#edit_form').submit(); }" class="btn btn-primary">Submit</button>
         </p>
-    </form>
+    </div>
+    <script>
+        $(function() {
+            var img_link = "{{ $post->img_link }}";
+
+            if(!img_link) {
+                $("#img_text").val("선택된 파일 없음");
+            }
+
+            $("#img").change(function (){
+                var file = this.files[0];
+
+                console.log(file.name);
+                $("#img_text").val(file.name);
+            })
+
+            $("#add_file").click(function() {
+                $("#img").click();
+            })
+        })
+    </script>
 @endsection
 
