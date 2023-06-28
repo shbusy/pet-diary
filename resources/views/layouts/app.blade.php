5a759c81d58d57050ebc28cfb870a79fcfbc6dea
limsuhyun <shbusy1234@gmail.com>
2023-06-29 01:44:49 +0900

    Fix: Mixed content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
         <script  src="http://code.jquery.com/jquery-latest.min.js"></script>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootswatch@5.3.0/dist/minty/bootstrap.min.css">
         <title>Pet-Diary</title>
