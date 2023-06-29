5ad45735a9fcb0571d872a566367c4152d71e45f
limsuhyun <shbusy1234@gmail.com>
2023-06-29 16:13:12 +0900

    Feat : edit header for mobile

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,29 +13,41 @@
         <div class="navbar navbar-expand-lg bg-light" data-bs-theme="light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/">Pet-Diary</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" onclick="$('#navbarColor03').toggle();" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarColor03">
                         <ul class="navbar-nav me-auto">
                     @auth
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('blogs.index') }}">MyFeed</a>
-                            </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('blogs.index') }}">MyFeed</a>
+                        </li>
                     @endauth
                         </ul>
 
                     @auth
-                        <form action="{{ route('logout') }}" method="POST">
+                        <form action="{{ route('logout') }}" method="POST" id="logout_form">
                             @csrf
-                            <button type="submit" style="margin-right: 5px" class="btn btn-outline-primary">logout</button>
                         </form>
                     @endauth
 
-                    @guest
-                        <button type="button" style="margin-right: 5px" class="btn btn-primary" onclick="location.href='{{ route('login') }}'">login</button>
-                        <button type="button" style="margin-right: 5px" class="btn btn-primary" onclick="location.href='{{ route('register') }}'">join</button>
-                    @endguest
+                    <form class="d-flex">
+                        <ul class="navbar-nav me-auto">
+                        @auth
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0);" onclick="$('#logout_form').submit();">logout</a>
+                            </li>
+                        @endauth
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0);" onclick="location.href='{{ route('login') }}'">login</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="javascript:void(0);" onclick="location.href='{{ route('register') }}'">join</a>
+                            </li>
+                        @endguest
+                        </ul>
+                    </form>
                 </div>
             </div>
         </div>
