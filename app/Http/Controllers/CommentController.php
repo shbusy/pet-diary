9a97a38590e1709c97e2ffe146926d899bde0e27
limsuhyun <shbusy1234@gmail.com>
2023-07-01 23:08:25 +0900

    feat : add store() for save comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,31 +5,35 @@ namespace App\Http\Controllers;
 use App\Models\Comment;
 use App\Http\Requests\StoreCommentRequest;
 use App\Http\Requests\UpdateCommentRequest;
+use App\Models\Post;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    // 댓글 작성
+    public function store(StoreCommentRequest $request, Post $post)
     {
-        //
-    }
+        // 작성자에 대한 정보
+        $user = $request->user();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreCommentRequest $request)
-    {
-        //
+        if (!$comment = $user->comments()->make($request->validated())) { // 의존성 해결을 위해 make 메소드를 사용
+            return with([
+                'msg' => '댓글 작성에 실패했습니다. 다시 시도해주세요',
+                'result' => 'f'
+            ]);
+        }
+
+        // 소속된 글 지정
+        if (!$post->comments()->save($comment)) { //save: 엘로퀀트 모델 인스턴스를 인자로 받는다
+            return with([
+                'msg' => '댓글 작성에 실패했습니다. 다시 시도해주세요',
+                'result' => 'f'
+            ]);
+        }
+
+        return with([
+           'result' => 's'
+        ]);
     }
 
     /**
@@ -40,25 +44,13 @@ class CommentController extends Controller
         //
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Comment $comment)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
+    // 댓글 수정
     public function update(UpdateCommentRequest $request, Comment $comment)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
+    // 댓글 삭제
     public function destroy(Comment $comment)
     {
         //
