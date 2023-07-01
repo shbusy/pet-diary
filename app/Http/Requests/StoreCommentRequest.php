65ced0215edbb4ef5c304d5f819ff715ade36982
limsuhyun <shbusy1234@gmail.com>
2023-07-01 23:11:55 +0900

    feat : add CommentStoreRequest

--- app/Http/Requests/StoreCommentRequest.php
+++ app/Http/Requests/StoreCommentRequest.php
@@ -11,7 +11,7 @@ class StoreCommentRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StoreCommentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'parent_id' => 'nullable|numeric|exists:comments,id',
+            'content' => 'required|string'
         ];
     }
 }
